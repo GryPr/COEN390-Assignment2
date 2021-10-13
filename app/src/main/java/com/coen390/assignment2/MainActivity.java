56a87c840c06e35e1b1f8af405b55f117a1d795d
KevinP <promkev@gmail.com>
2021-10-13 12:52:29 -0400

    Added fragment design

--- app/src/main/java/com/coen390/assignment2/MainActivity.java
+++ app/src/main/java/com/coen390/assignment2/MainActivity.java
@@ -3,13 +3,23 @@ package com.coen390.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView mainActivityList;
+    TextView mainActivityInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void setupUI() {
+        mainActivityList = findViewById(R.id.mainActivityList);
+        mainActivityInfo = findViewById(R.id.mainActivityInfo);
     }
 
     
--- app/src/main/java/com/coen390/assignment2/ProfileActivity.java
+++ app/src/main/java/com/coen390/assignment2/ProfileActivity.java
@@ -2,13 +2,29 @@ package com.coen390.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    protected String surname, name, id, gpa, dateCreated;
+    protected TextView userProfileInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile2);
+        setContentView(R.layout.activity_profile);
+    }
+
+    @SuppressLint("SetTextI18n")
+    protected void setupUP() {
+        userProfileInfo = findViewById(R.id.userProfileInfo);
+        userProfileInfo.setText("Surname: " + surname + "\n" +
+                "Name: " + name + "\n" +
+                "ID: " + id + "\n" +
+                "GPA: " + gpa + "\n" +
+                "Profile Created: " + dateCreated);
+
     }
 }
